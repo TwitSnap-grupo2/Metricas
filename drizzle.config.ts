c9f361b4a71554bf175daf50ce8c79ec1d5200ce
Gianni Boccazzi <gianniboccazzi@gmail.com>
2024-11-14 13:24:41 -0300

    fix dockerfile

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,19 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-import config from './src/utils/config';
+import { defineConfig } from "drizzle-kit";
+import config from "./src/utils/config";
+require("dotenv").config();
 
 export default defineConfig({
-  out: '../src/db/migrations',
-  schema: './src/db/schemas/*',
-  dialect: 'postgresql',
+  schema:
+    process.env.NODE_ENV === "production"
+      ? "./dist/src/db/schemas/*"
+      : "./src/db/schemas/*",
+  out:
+    process.env.NODE_ENV === "production"
+      ? "./dist/src/db/migrations"
+      : "./src/db/migrations",
   dbCredentials: {
     url: config.POSTGRES_URL as string,
   },
+  dialect: "postgresql",
   verbose: true,
 });
